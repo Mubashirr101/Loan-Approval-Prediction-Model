60eb2cb32ea12ca9a3225eacd124a79a1bd0cab3
Mubashir <zerex68@gmail.com>
2025-01-23 15:13:04 +0530

    model is save and is loaded in the app for usage logic

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import xgboost as xgb
 
 
 class LoanApprovalPredApp:
@@ -11,6 +12,11 @@ class LoanApprovalPredApp:
 
     def mainpage(self):
         st.write("Content")
+        xgb_model = xgb.Booster()
+        xgb_model.load_model("src/xgb_model.json")
+
+        # input
+        st.write("modelmade")
 
 
 if __name__ == "__main__":
